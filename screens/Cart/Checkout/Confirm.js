06997edaa0878709d9461130a546b26825837d61
thungcodejs <ptuanhungg@gmail.com>
2022-12-03 17:00:49 +0700

    fix checkout

---
+++ screens/Cart/Checkout/Confirm.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Text, View} from 'react-native';
+
+
+const ConfirmScreen = () => {
+  return (
+    <View>
+      <Text>
+        Confirm Screen
+      </Text>
+    </View>
+  )
+}
+
+export default ConfirmScreen;
